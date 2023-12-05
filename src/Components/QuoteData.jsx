9f42d5baae8659f636247793553e0f852276f846
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2023-12-05 15:16:32 -0700

    Function
    Tried making a function to refresh quotes

--- src/Components/QuoteData.jsx
+++ src/Components/QuoteData.jsx
@@ -16,12 +16,27 @@ const QuoteData = () => {
         })
     }, []);
 
+  // let getCounts = (count = 3)=>{
+  //   fetch(`https://api.quotable.io/quotes/random?limit=${count}`).then(response=>{
+  //       if(response.status >= 200 && response.status < 300){
+  //           return response.json();
+  //       }else{
+  //           throw new Error(response.statusText);
+  //       }
+  //   }).then(quote=>{
+  //       setQuotes(quote);
+  //   })
+  // }
+  // userName: universalUser
+  // password: universal1234
+
   return (
     <div>
       {quotes.map(quote=>{
         return (
           <div key={quote.id}>
-            <SingleQuote quote={quote}></SingleQuote>
+            <SingleQuote quote={quote}/>
+            {/* <button onClick={getCounts}>New Quotes</button> */}
           </div>
         )
       })}
--- src/Components/SingleQuote.jsx
+++ src/Components/SingleQuote.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const SingleQuote = ({quote}) => {
   return (
-    <>
+    <div>
         <h3>"{quote.content}" <span>- {quote.author}</span></h3>
-    </>
+    </div>
   )
 }
 
